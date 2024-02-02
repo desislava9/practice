2fea73dcde9a29ee6560237ec7792c1f1039c099
GoldieSpeaks <34451415+GoldieSpeaks@users.noreply.github.com>
2024-02-02 22:25:04 +0200

    d
    d

--- dom.ex/02. Pascal or Camel Case/solution.js
+++ dom.ex/02. Pascal or Camel Case/solution.js
@@ -1,17 +1,24 @@
 function solve() {
-  const resultRef = document.getElementById("result")
-  let text = document.getElementById("text").value;
+  const resultRef = document.getElementById("result");
+  let textValue = document.getElementById("text").value;
   let convention = document.getElementById("naming-convention").value;
-
+  textValue = textValue.toLowerCase();
   let result = "";
+  let textArr = textValue.split(" ");
 
   switch(convention) {
 case "Camel Case":
-  textArr = textValue.split(" ");
-  result = textArr.shift().toLoweCase();
+  result = textArr.shift();
   textArr.forEach(word => {
-    result += word[0].toUpperCase() = word.substring(1).toLoweCase();
+    result += word[0].toUpperCase() = word.substring(1).toLowerCase();
   });
+  break;
+  case "Pascal Case" :
+    textValue.forEach(word => {
+      result += word[0].toUpperCase() + word.substring(1);
+    });
+    break;
+    default : result = "Error!"
 
   }
   resultRef.textContent = result;
